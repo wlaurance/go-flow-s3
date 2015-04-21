c2f5105681c7b4299f282f5b09ea75fd591f5ecb
wlaurance <w.laurance@gmail.com>
2015-04-20 17:51:02 -0700

    Assuming we have a JPEG, get config and return height and width

--- stats.go
+++ stats.go
@@ -1,9 +1,23 @@
 package main
 
+import (
+	"bytes"
+	"image"
+	"image/jpeg"
+)
+
+func getImageConfigFromBytes(b []byte) image.Config {
+	config, err := jpeg.DecodeConfig(bytes.NewReader(b))
+	if err != nil {
+		panic(err)
+	}
+	return config
+}
+
 func GetWidthFromJpegBytes(b []byte) int {
-	return 0
+	return getImageConfigFromBytes(b).Width
 }
 
 func GetHeightFromJpegBytes(b []byte) int {
-	return 0
+	return getImageConfigFromBytes(b).Height
 }
